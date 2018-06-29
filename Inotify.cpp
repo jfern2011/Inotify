c8680c624275df2e9ab415a589a2f38f2ccbd110
jfern2011 <jfern2011@gmail.com>
2018-06-28 23:11:53 -0700

    Refactored this project

---
+++ Inotify.cpp
@@ -0,0 +1,166 @@
+/**
+ *  \file   Inotify.cpp
+ *  \author Jason Fernandez
+ *  \date   6/28/2018
+ *
+ *  https://github.com/jfern2011/Inotify
+ */
+
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include "Inotify.h"
+
+/**
+ * Constructor
+ */
+Inotify::Inotify() : _fd(), _raw(nullptr), _raw_len(0)
+{
+}
+
+/**
+ * Destructor
+ */
+Inotify::~Inotify()
+{
+	if (_raw) delete[] _raw;
+}
+
+/**
+ * Wrapper to the inotify_add_watch() API call
+ *
+ * @param[in] path See inotify_add_watch(2)
+ * @param[in] mask See inotify_add_watch(2)
+ *
+ * @return A unique watch descriptor
+ */
+int Inotify::add_watch(const std::string& path, uint32_t mask)
+{
+	return ::inotify_add_watch(_fd.get(), path.c_str(), mask);
+}
+
+/**
+ * Wrapper to the inotify_init1() API call
+ *
+ * @param[in] flags See inotify_init(2)
+ *
+ * @return True on success
+ */
+bool Inotify::init(int flags)
+{
+	_fd.reset( ::inotify_init1(flags) );
+	return _fd;
+}
+
+/**
+ *  Poll for events, forwarding them to consumers in the form of
+ *  \ref Event structs
+ *
+ * @param[in] timeout Wait at most this many milliseconds for
+ *                    events before returning. If negative, this
+ *                    may block indefinitely; if zero, this
+ *                    returns immediately, even if no events are
+ *                    available (returning true in that case)
+ *
+ * @return True on success
+ */
+bool Inotify::poll(int timeout)
+{
+	if (_fd.can_read(timeout))
+	{
+		int fsize = 0;
+		if (::ioctl(_fd.get(), FIONREAD, &fsize) < 0)
+			return false;
+
+		if (!_resize(fsize)) return false;
+
+		return _emit_data(fsize);
+	}
+
+	return true;
+}
+
+/**
+ * Wrapper to the inotify_rm_watch() API call
+ *
+ * @param[in] wd See inotify_rm_watch(2)
+ *
+ * @return True on success
+ */
+bool Inotify::rm_watch(int wd)
+{
+	return ::inotify_rm_watch(_fd.get(), wd) == 0;
+}
+
+/**
+ * Emit event notifications on \ref data_sig
+ *
+ * @note The number of \a bytes to read should resolve to an
+ *       integral number of events; in other words, there should
+ *       never be any leftover bytes after reading any number of
+ *       events. If this were to occur, those leftover bytes
+ *       would be discarded
+ *
+ * @param[in] bytes The number of bytes of data to read
+ *
+ * @return True on success
+ */
+bool Inotify::_emit_data(int bytes)
+{
+	if (data_sig.is_connected())
+	{
+		int nbytes = ::read( _fd.get(), _raw, bytes);
+		if (nbytes < 0) return false;
+
+		Event event;
+
+		for (char* ptr = _raw; ptr < _raw + bytes;)
+		{
+			struct inotify_event* evt_ptr =
+				static_cast<inotify_event*>(evt_ptr);
+
+			event.cookie = evt_ptr->cookie;
+			event.mask   = evt_ptr->mask;
+			event.name   = std::string(evt_ptr->name,
+									   evt_ptr->len);
+			event.wd     = evt_ptr->wd;
+
+			if ( !data_sig->raise(event) )
+				return false;
+
+			ptr += sizeof(struct inotify_event)
+					+ evt_ptr->len;
+		}
+	}
+
+	return true;
+}
+
+/**
+ * Size the internal buffer as needed to accommodate input
+ * from the inotify file descriptor. If the buffer is already
+ * sufficiently large, nothing is done. Otherwise, if dynamic
+ * allocation fails, the buffer retains its original state
+ *
+ * @param[in] new_size The new size, in bytes
+ *
+ * @return True on success
+ */
+bool Inotify::_resize(size_t new_size)
+{
+	if (new_size > _raw_len)
+	{
+		char* temp = _raw;
+
+		if (!(_raw = new char[ new_size ]))
+		{
+			_raw = temp; return false;
+		}
+
+		if (temp) delete[] temp;
+		
+		_raw_len = new_size;
+	}
+
+	return true;
+}
---
+++ Inotify.h
@@ -0,0 +1,96 @@
+/**
+ *  \file   Inotify.h
+ *  \author Jason Fernandez
+ *  \date   6/28/2018
+ *
+ *  https://github.com/jfern2011/Inotify
+ */
+
+#ifndef __INOTIFY_H__
+#define __INOTIFY_H__
+
+#include <string>
+
+#include "SharedFd.h"
+#include "signal/Signal.h"
+
+/**
+ * @class Inotify
+ *
+ * A wrapper for the Linux Inotify API. Unlike the API which requires
+ * reading from a raw file descriptor, an Inotify object is polled
+ * for events which are handled by user-defined routines. This avoids
+ * having to pull variable-sized inotify_event structs as described
+ * in the Inotify(7) page
+ */
+class Inotify
+{
+
+public:
+
+	/**
+	 * This is essentially an inotify_event struct, but replaces the
+	 * name/len fields with a std::string. When an Inotify object
+	 * is polled, any events that occured since the last \ref poll()
+	 * are emitted via the \ref data_sig
+	 */
+	struct Event
+	{
+		/** The watch descriptor  */
+		int         wd;
+
+		/** The mask of events    */
+		uint32_t    mask;
+
+		/** Cookie to sync events */
+		uint32_t    cookie;
+
+		/** Optional name */
+		std::string name;
+	};
+
+	Inotify();
+
+	~Inotify();
+
+	int add_watch(const std::string& path, uint32_t mask);
+
+	bool init(int flags = 0);
+
+	bool poll(int timeout = 0);
+
+	bool rm_watch(int wd);
+
+	/**
+	 * The signal raised when events are ready
+	 */
+	Signal::Signal<bool, const Event&>
+		data_sig;
+
+private:
+
+	bool _emit_data(int bytes);
+
+	bool _handle_except( const SharedFd& fd );
+
+	bool _handle_input ( const SharedFd& fd );
+
+	bool _resize(size_t new_size);
+
+	/**
+	 *  The inotify file descriptor
+	 */
+	SharedFd _fd;
+
+	/**
+	 * Stores file descriptor reads
+	 */
+	char* _raw;
+
+	/**
+	 * Size of _raw, in bytes
+	 */
+	size_t raw_len;
+};
+
+#endif
